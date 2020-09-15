75c051afebfb9838496563b31c1c994d75f9b8eb
yezii <1126yezy@gmail.com>
2020-09-15 19:12:07 +0900

    Defining Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,50 @@
-export const people = [
+let movies = [
+  {
+    id: 0,
+    name: "Avengers",
+    score: 9,
+  },
   {
     id: 1,
-    name: "yejiiha",
-    age: 24,
-    gender: "female",
+    name: "Aladin",
+    score: 9,
   },
   {
     id: 2,
-    name: "ans",
-    age: 20,
-    gender: "male",
+    name: "Harry potter",
+    score: 10,
   },
   {
     id: 3,
-    name: "rascal",
-    age: 23,
-    gender: "male",
-  },
-  {
-    id: 4,
-    name: "super",
-    age: 20,
-    gender: "female",
+    name: "Frozen",
+    score: 8,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === String(id));
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  console.log(movies);
+  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,13 @@
-import { people, getById } from "./db";
+import { getMovies, getById, addMovie } from "./db";
 
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, { id }) => getById(id),
+    movies: () => getMovies,
+    movie: (_, { id }) => getById(id),
   },
+  // Mutation: {
+  //   addMovie: (_, { name, score }) => addMovie(name, score),
+  // },
 };
 
 export default resolvers;
