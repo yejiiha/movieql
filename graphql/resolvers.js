079f7cffa66eee1d4de1e9f3fa8bfd9c5207d14d
yezii <1126yezy@gmail.com>
2020-09-15 20:41:41 +0900

    Add and Delete Mutations

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,14 @@
-import { getMovies, getById, addMovie } from "./db";
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies,
+    movies: () => getMovies(),
     movie: (_, { id }) => getById(id),
   },
-  // Mutation: {
-  //   addMovie: (_, { name, score }) => addMovie(name, score),
-  // },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
+  },
 };
 
 export default resolvers;
